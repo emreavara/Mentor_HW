aa21d78146bf2761c284db275f99a20d4148edb8
Bekir Kurubas <bekirkurubas6767@gmail.com>
2023-09-29 21:03:05 +0200

    pull request örnek dosya olusturuldu

---
+++ pullreq örnek.js
@@ -0,0 +1,9 @@
+ const text = "This is an example sentence.";
+// verilen metnin 3 ten 10'a kadar olan karakterlerini ekrana yazdirma
+
+const baslangicIndex = 3;
+const bitisIndex = 10;
+
+const kesilmisMetin = text.substring(baslangicIndex,bitisIndex);
+
+console.log(kesilmisMetin);
