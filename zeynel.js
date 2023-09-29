670659983770411b1d5c7428d7aa8d48dd59ea9c
zynll45 <zeynel.sonmez@hicoders.ch>
2023-09-29 22:22:34 +0200

    adi degistir

---
+++ zeynel.js
@@ -0,0 +1,8 @@
+const text = "This is an example sentence.";
+const slicedText = text.slice(3, 10);
+
+console.log(slicedText);
+
+// const text = "This is an example sentence.";
+// const newText = text.replace("example", "english");
+// console.log(newText);
